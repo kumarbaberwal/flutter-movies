6d912a8a632a175c873a11051d554e81066375a9
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-10-29 15:17:51 +0530

    movie entity

---
+++ lib/domain/movie/entities/movie_entity.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class MovieEntity {
+  final String? backdropPath;
+  final int? id;
+  final String? title;
+  final String? originalTitle;
+  final String? overview;
+  final String? posterPath;
+  final String? mediaType;
+  final bool? adult;
+  final String? originalLanguage;
+  final List<int> genreIds;
+  final double? popularity;
+  final DateTime? releaseDate;
+  final bool? video;
+  final double? voteAverage;
+  final int? voteCount;
+  MovieEntity({
+    this.backdropPath,
+    this.id,
+    this.title,
+    this.originalTitle,
+    this.overview,
+    this.posterPath,
+    this.mediaType,
+    this.adult,
+    this.originalLanguage,
+    required this.genreIds,
+    this.popularity,
+    this.releaseDate,
+    this.video,
+    this.voteAverage,
+    this.voteCount,
+  });
+}
