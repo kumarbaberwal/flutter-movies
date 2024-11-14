51b8dbfad8775bd8dd9fc1b497b68cc474feabb6
kumarbaberwal <niteshkumarghn1@gmail.com>
2024-11-14 23:35:40 +0530

    trailer mapper

---
+++ lib/common/helper/mapper/trailer_mapper.dart
@@ -0,0 +1,18 @@
+import 'package:movies/core/entities/trailer_entity.dart';
+import 'package:movies/core/models/trailer_model.dart';
+
+class TrailerMapper {
+  static TrailerEntity toEntity(TrailerModel trailerModel) {
+    return TrailerEntity(
+        iso6391: trailerModel.iso6391,
+        iso31661: trailerModel.iso31661,
+        name: trailerModel.name,
+        key: trailerModel.key,
+        site: trailerModel.site,
+        size: trailerModel.size,
+        type: trailerModel.type,
+        official: trailerModel.official,
+        publishedAt: trailerModel.publishedAt,
+        id: trailerModel.id);
+  }
+}
